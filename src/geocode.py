1662fa6b3ecc881541cdf5342a89cba19179ab64
Brandon Heller <brandonh@stanford.edu>
2011-06-03 14:55:25 -0700

    Make weighted OS3E graph automatically

---
+++ src/geocode.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+'''Utilities for geocoding city names using PlaceFinder.
+
+Example browser request:
+http://where.yahooapis.com/geocode?q=Portland&appid=faypaS6k&flags=J
+
+Python Yahoo! REST examples:
+http://developer.yahoo.com/python/python-rest.html
+
+geopy:
+http://groups.google.com/group/geopy/browse_thread/thread/d06d5a2aefb0e63b
+'''
+import json
+import urllib
+
+import httplib2
+
+
+def get_lat_long(location, appid = 'faypaS6k'):
+    http = httplib2.Http()
+    
+    url = 'http://where.yahooapis.com/geocode?'
+
+    params = urllib.urlencode({
+        'appid': appid,
+        'q': location,
+        'flags': 'JC'
+    })
+
+    response, content = http.request(url + params, 'GET')
+    print location, response, content
+    content_json = json.loads(content)
+    resultset = content_json["ResultSet"]
+
+    assert resultset["Error"] == 0
+    assert resultset["Found"] == 1
+    result = resultset["Results"][0]
+
+    return {
+            "latitude": result["latitude"],
+            "longitude": result["longitude"]
+    }
---
+++ src/os3e_weighted.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+'''Find geocodes for each node name in the OS3e graph using PlaceFinder.'''
+import os
+
+# From http://hoegners.de/Maxi/geo/
+import geo
+# To test, go to http://www.daftlogic.com/projects-google-maps-distance-calculator.htm
+# and run this code - see if the Portland - SanFran distance is ~533 miles.
+# portland = geo.xyz(45.5, -122.67)
+# sanfran = geo.xyz(37.777, -122.419)
+METERS_TO_MILES = 0.000621371192
+# print geo.distance(portland, sanfran) * METERS_TO_MILES
+
+from file_libs import write_json_file, read_json_file
+from geocode import get_lat_long
+from topo.os3e import OS3EGraph
+
+LATLONG_FILE = "geo/os3e_latlong.json"
+
+def lat_long_pair(node):
+    return (float(node["latitude"]), float(node["longitude"]))
+
+def dist_in_miles(data, src, dst):
+    '''Given a dict of names and location data, compute mileage between.'''
+    src_pair = lat_long_pair(data[src])
+    src_loc = geo.xyz(src_pair[0], src_pair[1])
+    dst_pair = lat_long_pair(data[dst])
+    dst_loc = geo.xyz(dst_pair[0], dst_pair[1])
+    return geo.distance(src_loc, dst_loc) * METERS_TO_MILES
+
+def OS3EWeightedGraph():
+
+    data = {}
+    g = OS3EGraph()
+
+    # Get locations
+    if os.path.isfile(LATLONG_FILE):
+        print "Using existing lat/long file"
+        data = read_json_file(LATLONG_FILE)
+    else:
+        print "Generating new lat/long file"
+        for n in g.nodes():
+            data[n] = get_lat_long(n)
+        write_json_file(LATLONG_FILE, data)
+
+    # Append weights
+    for src, dst in g.edges():
+        g[src][dst]["weight"] = dist_in_miles(data, src, dst)
+        print "%s to %s: %s" % (src, dst, g[src][dst]["weight"])
+
+
+g = OS3EWeightedGraph()
