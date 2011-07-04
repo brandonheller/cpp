a6dd349d37971e2549ed24eb19952600bf39d694
Brandon Heller <brandonh@stanford.edu>
2011-07-04 03:01:26 -0700

    Add paper-creation script

---
+++ src/make_paper.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Generate all OS3E graphs:
+C=5
+METRICS="latency wc_latency"
+./metrics.py --from_start ${C} --lat_metrics -w --write_dist --write_combos
+./plot_cdfs.py -i data_out/os3e_weighted_${C}_to_0.json -w --metric_list ${METRICS}
+./plot_ranges.py -i data_out/os3e_weighted_${C}_to_0.json -w --metric_list ${METRICS}
+./map_combos.py -i data_out/os3e_weighted_${C}_to_0.json -w
