4ec7937cf4370192fa845b47d39a1825b4ba1976
Brandon Heller <brandonh@stanford.edu>
2011-07-04 17:00:54 -0700

    Note major job on Rhone

--- src/make_paper.sh
+++ src/make_paper.sh
@@ -6,3 +6,7 @@ METRICS="latency,wc_latency"
 ./plot_cdfs.py -i data_out/os3e_weighted_${C}_to_0.json -w --metric_list="${METRICS}"
 ./plot_ranges.py -i data_out/os3e_weighted_${C}_to_0.json -w --metric_list="${METRICS}"
 ./map_combos.py -i data_out/os3e_weighted_${C}_to_0.json -w
+
+# BIG job (on Rhone)
+#time ./metrics.py --from_start 12 --from_end 12 --lat_metrics --weighted -w --processes 8
+#./plot_ranges.py -i data_out/os3e_weighted_12_to_12.json -w --metric_list="${METRICS}"
