f1dd9561e56971069c5aa2f90603b96835722a46
Brandon Heller <brandonh@stanford.edu>
2011-07-05 17:12:08 -0700

    Add more range plot types

--- src/plot_ranges.py
+++ src/plot_ranges.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 '''Plot ranges, where each series is a # of controllers.'''
 import lib.plot as plot
+import math
 
-PLOTS = ['ranges', 'ratios', 'durations', 'bc_abs', 'bc_rel']
+
+PLOTS = ['ranges', 'ratios', 'durations', 'bc_abs', 'bc_rel', 'bc_rel_sq', 'abs_benefit', 'miles_cost']
+
 
 def divide(left, right):
     if right == 0:
@@ -36,6 +39,18 @@ def do_ranges():
                         options.write, ext = options.ext,
                         xlabel = 'number of controllers',
                         ylabel = metric + " (miles)")
+
+        if 'miles_cost' in PLOTS:
+            print "plotting ranges"
+            aspect_colors = {'miles_cost': 'rx'}
+            aspect_fcns = {'miles_cost': (lambda g, d, m: d[m]['lowest']  / float(g))}
+            aspects = aspect_fcns.keys()
+            plot.ranges(stats, metric, aspects, aspect_colors, aspect_fcns,
+                        "linear", "linear", None, None, write_filepath + '_miles_cost',
+                        options.write, ext = options.ext,
+                        xlabel = 'number of controllers',
+                        ylabel = metric + " miles over cost")
+
 
         if 'ratios' in PLOTS:
             aspect_colors = {'highest': 'rx',
@@ -81,7 +96,43 @@ def do_ranges():
                         "linear", "linear", None, None, write_filepath + '_bc_rel',
                         options.write, ext = options.ext,
                         xlabel = 'number of controllers',
-                        ylabel = metric + " benefit/cost (ratio)", min_x = 1)
+                        ylabel = metric + " benefit/cost (ratio)", min_x = 1,
+                        max_x = options.maxx, max_y = options.maxy)
+
+        if 'bc_rel_sq' in PLOTS:
+            aspect_colors = {'bc_rel_sq': 'rx'}
+            value_one = stats['data'][sorted(stats['group'])[0]][metric]['lowest']
+            aspect_fcns = {'bc_rel_sq': (lambda g, d, m: divide(value_one, float(d[m]['lowest'])) / (float(g) ** 2))}
+            aspects = aspect_fcns.keys()
+            plot.ranges(stats, metric, aspects, aspect_colors, aspect_fcns,
+                        "linear", "linear", None, None, write_filepath + '_bc_rel_sq',
+                        options.write, ext = options.ext,
+                        xlabel = 'number of controllers',
+                        ylabel = metric + " benefit/cost (ratio)", min_x = 1,
+                        max_x = options.maxx, max_y = options.maxy)
+
+        if 'abs_benefit' in PLOTS:
+            aspect_colors = {'abs_benefit': 'rx'}
+            groups = sorted([int(g) for g in stats['group']])
+            abs_benefits = {}
+            for i, g in enumerate(groups):
+                if i == 0:
+                    abs_benefits[g] = 0
+                else:
+                    now = stats['data'][str(g)][metric]['lowest']
+                    if str(g - 1) not in stats['data']:
+                        prev = now
+                    else:
+                        prev = stats['data'][str(g - 1)][metric]['lowest']
+                    abs_benefits[g] = prev - now
+            aspect_fcns = {'abs_benefit': (lambda g, d, m: abs_benefits[g])}
+            aspects = aspect_fcns.keys()
+            plot.ranges(stats, metric, aspects, aspect_colors, aspect_fcns,
+                        "linear", "linear", None, None, write_filepath + '_abs_benefits',
+                        options.write, ext = options.ext,
+                        xlabel = 'number of controllers',
+                        ylabel = metric + " inc. abs_benefit (miles)", min_x = 2)
+
 
     if not options.write:
         plot.show()
