cf3855db6ac8943d17d0f2db3cec0b1afd999540
Brandon Heller <brandonh@stanford.edu>
2011-07-06 15:48:40 -0700

    Add saved data for OS3E.

--- src/make_paper.sh
+++ src/make_paper.sh
@@ -3,7 +3,7 @@
 C=5
 METRICS="latency,wc_latency"
 EXT=pdf
-#./metrics.py --from_start ${C} --lat_metrics -w --write_dist --write_combos
+./metrics.py --from_start ${C} --lat_metrics -w --write_dist --write_combos
 ./plot_cdfs.py -i data_out/os3e_weighted_${C}_to_0.json -w --metric_list="${METRICS}" -e ${EXT}
 ./plot_ranges.py -i data_out/os3e_weighted_${C}_to_0.json -w --metric_list="${METRICS}" -e ${EXT}
 # generate PNGs for cloud - way too many points otherwise
@@ -13,4 +13,4 @@ EXT=pdf
 
 # BIG job (on Rhone)
 #time ./metrics.py --from_start 12 --from_end 12 --lat_metrics --weighted -w --processes 8
-./plot_ranges.py -i data_out/os3e_weighted_12_to_12.json -w --metric_list="latency,wc_latency" --maxx 12 --maxy 1  -e ${EXT}
+./plot_ranges.py -i data_saved/os3e_weighted_12_to_12.json -w --metric_list="latency,wc_latency" --maxx 12 --maxy 1  -e ${EXT}
