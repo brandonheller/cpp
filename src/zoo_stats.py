84fbf7f8f6ef33309f6bf7cf06fadd7111a38c22
Brandon Heller <brandonh@stanford.edu>
2011-07-08 14:47:31 -0700

    Move to a more functional style of parsing Zoo topos to help code reuse

--- src/zoo_stats.py
+++ src/zoo_stats.py
@@ -1,56 +1,49 @@
 #!/usr/bin/env python
-import os
 from operator import attrgetter
 import networkx as nx
 
 from topo_lib import get_topo_graph, has_weights, total_weight
+from zoo_tools import for_each_zoo_topo
 
 ZOO_DIR = 'zoo'
 
-MAX = None
-NUM = 20
+MAX = 20
+NUM = 20  # Number of topologies to print for each list at the end
 
 class GraphData(object):
-    def __init__(self, name, n, e, ad, ic, c):
+    def __init__(self, name, n, e, ad, ic, c, w):
         self.name = name
         self.n = n
         self.e = e
         self.ad = ad
         self.ic = ic
         self.c = c
+        self.w = w
 
     def __repr__(self):
-        return repr((self.name, self.n, self.e, self.ad, self.ic, self.c))
+        return repr((self.name, self.n, self.e, self.ad, self.ic, self.c,
+                     self.w))
 
 
 if __name__ == '__main__':
+
     data = []
-    files = os.listdir(ZOO_DIR)
-    # Ignore files starting with a .
-    topos = [d.split(".")[0] for d in files if d != "" and d[0] != '.' and 'cache' not in d and '.gml' in d]
-    # Extract to just names
-    for i, topo in enumerate(topos):
-        if MAX != None and i == MAX:
-            break
-        g = get_topo_graph(topo)
-        if not g:
-            pass
-            #print "graph with no geos: %s" % topo
-        else:
-            print ">>>%s: %s of %s" % (topo, i, len(topos))
-            if has_weights(g):
-                w = total_weight(g)
-            else:
-                w = 0.0
-            de = GraphData(name = topo,
-                           n = g.number_of_nodes(),
-                           e = g.number_of_edges(),
-                           ad = g.number_of_edges() / float(g.number_of_nodes()),
-                           ic = nx.is_connected(g),
-                           c = nx.number_connected_components(g))
-            data.append(de)
-            print "\t" + str(de)
-    
+
+    def add_to_data(name, g, i, t, data):
+        g = get_topo_graph(name)
+        print ">>>%s: %s of %s" % (name, i, t)
+        de = GraphData(name = name,
+                       n = g.number_of_nodes(),
+                       e = g.number_of_edges(),
+                       ad = g.number_of_edges() / float(g.number_of_nodes()),
+                       ic = nx.is_connected(g),
+                       c = nx.number_connected_components(g),
+                       w = total_weight(g) if has_weights(g) else 0)
+        data.append(de)
+        print "\t" + str(de)
+
+    for_each_zoo_topo(add_to_data, data, MAX)
+
     keys = [d for d in dir(data[0]) if "__" not in d and d != 'name']
     for key in keys:
         print key
---
+++ src/zoo_tools.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os
+
+from topo_lib import get_topo_graph
+
+ZOO_DIR = 'zoo'
+
+
+def zoo_topos():
+    files = os.listdir(ZOO_DIR)
+    # Ignore files starting with a .
+    topos = [d.split(".")[0] for d in files if d != "" and d[0] != '.' and 'cache' not in d and '.gml' in d]
+    return topos
+
+
+def for_each_zoo_topo(fcn, data, max_iters = None):
+    '''Execute the specified function for each topology in the zoo.
+    
+    @param fcn: function accepting the following params:
+        @param name: topology name
+        @param g: topology graph
+        @param i: index
+        @param t: total topologies in the zoo
+        @param data: arbitrary object
+    @data: data pass-through
+    @param max_iters: maximum number of topologies to use (for debug)
+    '''
+    # Extract to just names
+    topos = zoo_topos()
+    t = len(topos)
+    for i, topo in enumerate(topos):
+        if not max == None and i >= max_iters:
+            break
+        g = get_topo_graph(topo)
+        fcn(topo, g, i, t, data)
