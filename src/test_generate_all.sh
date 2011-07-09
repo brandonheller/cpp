32222812d4732efbbee134d28b446114f46912c2
Brandon Heller <brandonh@stanford.edu>
2011-07-09 03:21:35 -0700

    Add more scripts for generating lots of images

---
+++ src/test_generate_all.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Generate everything for all topologies that we can geocode.
+TOPO=os3e
+C=3
+EXT=pdf
+MAX=5  # add --max $MAX at the end of generate.py to debug only a few topos. 
+CS='3 4'
+for C in ${CS}
+do
+  echo "writing all for num controllers ${C}"
+  ./generate.py --all_topos --from_start ${C} --lat_metrics -w --write_dist --write_combos -w -e ${EXT} --max ${MAX}
+done
