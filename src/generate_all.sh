2efa8b58b26e49389c327ba51b5c4f35e4fd4c98
Brandon Heller <brandonh@stanford.edu>
2011-07-09 03:52:23 -0700

    Note use of 8 processes on Rhone

--- src/generate_all.sh
+++ src/generate_all.sh
@@ -4,10 +4,11 @@
 TOPO=os3e
 C=3
 EXT=pdf
+P=8  # proceses: 8 on Rhone, otherwise 4
 #MAX=5  # add --max $MAX at the end of generate.py to debug only a few topos. 
 CS='3 4 5 6 7'
 for C in ${CS}
 do
   echo "writing all for num controllers ${C}"
-  ./generate.py --all_topos --from_start ${C} --lat_metrics -w --write_dist --write_combos -w -e ${EXT}
+  ./generate.py --all_topos --from_start ${C} --lat_metrics -w --write_dist --write_combos -w -e ${EXT} --processes ${P}
 done
