dd1e34cc0d210974de7d40242e4cadd49e43c540
Brandon Heller <brandonh@stanford.edu>
2011-07-11 00:38:12 -0700

    Add changes to generate_all

--- src/generate_all.sh
+++ src/generate_all.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 # Generate everything for all topologies that we can geocode.
-# To run: time ./generate_all.sh > generate_all_log
+# To run: time ./generate_all.sh > generate_all_log &
+# To view: tail -n20 -f generate_all_log
 TOPO=os3e
 EXT=pdf
 #MAX=5  # add --max $MAX at the end of generate.py to debug only a few topos. 
-CS='4 5 6 7'
+CS='4'
+FORCE='-f'
+MP='--no-multiprocess'
+OPS='metrics'
 for C in ${CS}
 do
   echo "writing all for num controllers ${C}"
   echo "*********************"
-  ./generate.py --all_topos --from_start ${C} --lat_metrics -w --write_dist --write_combos -w -e ${EXT}
+  ./generate.py --all_topos --from_start ${C} --lat_metrics -w --write_dist --write_combos -w -e ${EXT} ${FORCE} ${MP} --operation_list ${OPS}
 done
