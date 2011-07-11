b8b16d11bb52022266310718f6730bac2a6c3012
Brandon Heller <brandonh@stanford.edu>
2011-07-11 15:56:42 -0700

    More flexible output for analyze script

--- src/zoo_analyze.py
+++ src/zoo_analyze.py
@@ -6,21 +6,28 @@ from topo_lib import get_topo_graph, has_weights, total_weight
 
 import sys
 
+COMPACT = True
+
 
 def print_topo_stats(name, g):
-    print "topo name: %s" % name
     n = g.number_of_nodes()
-    print "number of nodes: %s" % n
     e = g.number_of_edges()
-    print "number of edges: %s" % e
     ad = 2.0 * g.number_of_edges() / float(g.number_of_nodes())
-    print "average degree: %s" % ad
     ic = nx.is_connected(g)
-    print "is connected: %s" % ic
     c = nx.number_connected_components(g)
-    print "connected components: %s" % c
     w = total_weight(g) if has_weights(g) else 0
-    print "total weight: %.1f" % w
+    if COMPACT:
+        print "%s: %s %s %.1f %s %s %.1f" % (name, n, e, ad, ic, c, w)
+    else:
+        print "***********"
+        print "topo name: %s" % name
+        print "number of nodes: %s" % n
+        print "number of edges: %s" % e
+        print "average degree: %.1f" % ad
+        print "is connected: %s" % ic
+        print "connected components: %s" % c
+        print "total weight: %.1f" % w
+
 
 if __name__ == '__main__':
 
@@ -28,7 +35,6 @@ if __name__ == '__main__':
         print "please provide a topology name"
         exit()
     for topo in sys.argv[1:]:
-        print "***********"
         g = get_topo_graph(topo)
         print_topo_stats(topo, g)
 
