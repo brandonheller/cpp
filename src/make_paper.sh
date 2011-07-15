7a1fa31ce787fc4f6c163c23f72326e44f21e8cf
Brandon Heller <brandonh@stanford.edu>
2011-07-15 15:31:56 -0700

    Append lines to create merged graphs

--- src/make_paper.sh
+++ src/make_paper.sh
@@ -3,15 +3,38 @@
 
 C=5
 EXT=pdf
+# Add FORCE to forcibly regenerate metrics.
+# Only needed when the data schema or set of input topos changes.
 #FORCE='-f'
 
-# Generate all for OS3E topology
+# Generate all latency metrics data for OS3E topology
 TOPO=os3e
 ./generate.py --topo ${TOPO} --from_start ${C} --lat_metrics -w --write_dist --write_combos -w -e ${EXT} ${FORCE}
 
+# Generate mini topology images for OS3 topology
 ./map_combos.py -i data_out/${TOPO}/${C}_to_0.json --from_start ${C} --lat_metrics -w --write_dist --write_combos -e ${EXT}
 
 # Generate special big ones for OS3E
 # BIG job (on Rhone)
 #time ./metrics.py --from_start 12 --from_end 12 --lat_metrics --weighted -w
 ./plot_ranges.py -i data_out/os3e/12_to_12.json -w --lat_metrics --maxx 12 --maxy 1  -e ${EXT}
+
+# Requires all output from Rhone for up to k = 4.  To get such output, run:
+#   cd cc/src
+#   time rsync -v rhone:~/cc/src/data_out .
+# Don't use multiprocessing; MPL backend is not thread-safe at the moment.
+MP='--no-multiprocess'
+# Doesn't actually do anything in generate_merged, but prevents metrics from getting built.
+OPS='--operation_list pareto, ranges'
+
+# Generate all ranges plots
+PLOT_LIST='--plot_list ranges_lowest,ratios_all,ratios_mean,bc_rel'
+# For now, only go up to 8.
+C=8
+./generate_merged.py --all_topos --from_start ${C} --lat_metrics -w -e ${EXT} ${FORCE} ${MP} ${OPS} ${PLOT_LIST}
+
+# Generate tradeoffs output
+# Only go up to 4, because going to 5 requires > 1 GB of RAM.
+C=4
+PLOT_LIST='--plot_list pareto_max,pareto_max_bw'
+./generate_merged.py --all_topos --from_start ${C} --lat_metrics -w -e ${EXT} ${FORCE} ${MP} ${OPS} ${PLOT_LIST}
--- src/test_gen_ranges_merged.sh
+++ src/test_gen_ranges_merged.sh
@@ -9,24 +9,26 @@ OPS='--operation_list pareto'
 FORCE='--force'
 METRICS='--lat_metrics'
 
+PLOT_LIST='--plot_list pareto_max,pareto_max_bw'
+
 # Should generate an identical plot to generate.py w/operations_list='ranges'
 # data_vis/merged/4_to_0_latency_ranges.pdf
 # data_vis/Aarnet/4_to_0_latency_ranges.pdf
 GROUP='--topo_group test1'
-#./generate_merged.py ${GROUP} ${C} ${METRICS} -w -e ${EXT} ${FORCE} ${MP} ${OPS}
+#./generate_merged.py ${GROUP} ${C} ${METRICS} -w -e ${EXT} ${FORCE} ${MP} ${OPS} ${PLOT_LIST}
 
 # Should generate combination of two:
 # data_vis/merged/4_to_0_latency_ranges.pdf
 # data_vis/Aarnet/4_to_0_latency_ranges.pdf
 # data_vis/Abilene/4_to_0_latency_ranges.pdf
 GROUP='--topo_group test2'
-#./generate_merged.py ${GROUP} ${C} ${METRICS} -w -e ${EXT} ${FORCE} ${MP} ${OPS}
+./generate_merged.py ${GROUP} ${C} ${METRICS} -w -e ${EXT} ${FORCE} ${MP} ${OPS} ${PLOT_LIST}
 
 TOPOS='--all_topos'
-./generate_merged.py ${TOPOS} ${C} ${METRICS} -w -e ${EXT} ${FORCE} ${MP} ${OPS}
+#./generate_merged.py ${TOPOS} ${C} ${METRICS} -w -e ${EXT} ${FORCE} ${MP} ${OPS} ${PLOT_LIST}
 
 # Should yield more interesting data:
 C='--from_start 8'
 #MAX='--max 8'
 #METRICS='--metric latency'
-#./generate_merged.py ${TOPOS} ${C} ${METRICS} -w -e ${EXT} ${FORCE} ${MP} ${OPS} ${MAX}
+#./generate_merged.py ${TOPOS} ${C} ${METRICS} -w -e ${EXT} ${FORCE} ${MP} ${OPS} ${MAX} ${PLOT_LIST}
